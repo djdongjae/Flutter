2dd8b5dc33be1704efd9b19b5a9f512da03e6adf
djdongjae <isf1999@naver.com>
2023-04-05 18:00:04 +0900

    tab_list 수업

---
+++ tab_listview_test/android/app/src/main/java/com/example/tab_listview_test/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.tab_listview_test;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ tab_listview_test/lib/second_page.dart
@@ -0,0 +1,12 @@
+class SecondPage extends StatefulWidget {
+  final String title;
+
+  const SecondPage({super.key, required this.title});
+
+  @override
+  State<SecondPage> createState => _SecondPage();
+
+
+}
+
+class _FirstPage extends State
