52817d607e0543a295f0366af664658d0fd97976
djdongjae <isf1999@naver.com>
2023-06-06 23:08:18 +0900

    modified: lib/main.dart

--- sql_example_2023_06_04_1037i/lib/main.dart
+++ sql_example_2023_06_04_1037i/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,17 +9,40 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Future<Database> database = initDatabase();
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: DatabaseApp(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => DatabaseApp(database),
+        '/add': (context) => AddTodoApp(database)
+      },
+    );
+  }
+
+  Future<Database> initDatabase() async {
+    return openDatabase(
+      join(await getDatabasesPath(), 'todo_database.db'),
+    onCreate: (db, version) {
+        return db.execute(
+        "CREATE TABLE todos(id INTEGER PRIMARY KEY AUTOINCREMENT, "
+            "title TEXT, content TEXT, active INTEGER)",
+        );
+    },
+    version: 1,
     );
   }
 }
 
 class DatabaseApp extends StatefulWidget {
+  final Future<Database> db;
+  DatabaseApp(this.db);
+
   @override
   State<StatefulWidget> createState() => _DatabaseApp();
 }
@@ -30,10 +55,12 @@ class _DatabaseApp extends State<DatabaseApp> {
       appBar: AppBar(title: Text('Database Example'),),
       body: Container(),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          final todo = await Na
+        onPressed: () async {
+          final todo = await Navigator.of(context).pushNamed('/add');
         },
+        child: Icon(Icons.add)
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
